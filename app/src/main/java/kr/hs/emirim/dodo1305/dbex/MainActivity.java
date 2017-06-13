8ce12198d15ab3bcfb6afc9ddfdc596fc745b20a
dodo1305 <dodo1305@naver.com>
2017-06-13 20:05:10 +0900

    #2 Extends so SQLiteOpenHelper class

--- app/src/main/java/kr/hs/emirim/dodo1305/dbex/MainActivity.java
+++ app/src/main/java/kr/hs/emirim/dodo1305/dbex/MainActivity.java
@@ -1,5 +1,8 @@
 package kr.hs.emirim.dodo1305.dbex;
 
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -10,4 +13,25 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    class MyDBHelper extends SQLiteOpenHelper{
+        //idolDB라는 이름의 데이터베이스가 생성된다.
+        public MyDBHelper(Context context){
+            super(context, "idolDB", null,1);
+        }
+        //idolTable이라는 이름의 테이블 생성
+        @Override
+        public void onCreate(SQLiteDatabase db) {
+            String sql ="create table idolTable(idolName text not null primary key, idolCount integer)";
+            db.execSQL(sql);
+        }
+        //이미 idolTable이 존재한다면 기존의 테이블을 삭제하고 새로 테이블 만들 때 호출
+        @Override
+        public void onUpgrade(SQLiteDatabase db, int i, int i1) {
+            String sql ="drop table if exist idolTable";
+            db.execSQL(sql);
+            onCreate(db);
+        }
+    }
+
 }
